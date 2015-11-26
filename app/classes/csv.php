3601ccd600c2e285925eb49e969828773b1acda0
juanantoniofr <juanantonio.fr@gmail.com>
2015-11-26 13:13:12 +0100

    sgr

---
+++ app/classes/csv.php
@@ -0,0 +1,87 @@
+<?php
+
+class csv{
+
+	private $columns = array(	'F_HASTA' 			=>	'0', //fecha inicio
+								'F_DESDE'			=>	'1', //fecha fin
+								'AUL_CODNUM'		=>	'2',
+								'ID_LUGAR'			=>	'3', //identifcador de lugar
+								'DESTPA'			=>	'4',
+								'DESAUL'			=>	'5',
+								'ASS_CODNUM1'		=>	'6',
+								'COD_DIA_SEMANA'	=>	'7',
+								'INI'				=>	'8', //hora inicio
+								'FIN'				=>	'9', //hora fin
+								'COD_DIA'			=>	'10', //día de la semana 1->lunes, .... 5->viernes
+								'ASIGNATURA'		=>	'11',
+								'COD'				=>	'12',
+								'PER_CODNUM1'		=>	'13',
+								'HOR_CODNUM1'		=>	'14',
+								'DES_GRP'			=>	'15',
+								'F_DESDE_HORARIO1'	=>	'16',//fecha inicio!!
+								'F_HASTA_HORARIO1'	=>	'17',//fecha fin!!
+								'EJE_CODNUM1'		=>	'18',
+								'FRN_CODNUM1'		=>	'19',
+								'DIA_SEMANA'		=>	'20',
+								'DIA'				=>	'21',
+								'EJE_CODNUM'		=>	'22',
+								'HOR_CODNUM'		=>	'23',
+								'PER_CODNUM'		=>	'24',
+								'DSM_CODNUM'		=>	'25',
+								'FRN_CODNUM'		=>	'26',
+								'NOMCOM'			=>	'27', //Profesor
+								 );
+
+	private $columnValidas = array(	'ID_LUGAR',		//identificador de lugar
+									'F_DESDE_HORARIO1',		//fecha fin
+									'F_HASTA_HORARIO1',		//fecha inicio
+									'DIA_SEMANA',	//Día de la semana (texto)
+									'INI',			//hora inicio
+									'FIN',			//hora fin
+									'DESAUL',		//lugar
+									'ASIGNATURA',	//Asignatura
+									'NOMCOM', 		//Profesor
+									'COD_DIA_SEMANA', //dia de la semana numñerico, lunes->1, martes->2,...									
+								 );
+
+	private $errors = array (	'noexistelugar'		=> array(),
+								'formatonovalido'	=> array(),
+								'haysolapamiento'	=> array());
+
+		
+
+	public function __construct(){
+
+	}
+
+	public function getNumColumnIdLugar(){
+
+		return $this->columns['ID_LUGAR'];
+		
+	}
+
+	
+
+	public function filterFila($fila){
+		
+		$result = array();
+
+		foreach ($this->columnValidas as $keyValida) {
+			$numColumn = $this->getnumColumn($keyValida); //obtengo el indice de una columna valida
+			$result[$keyValida] = $fila[$numColumn];
+		}
+		
+		return $result;
+
+	}
+
+	private function getnumColumn($key){
+		$numColumn = '';
+
+		$numColumn = $this->columns[$key];
+
+		return $numColumn; 
+	}
+}
+
+?>
---
+++ app/classes/sgrMail.php
@@ -0,0 +1,200 @@
+<?php
+
+class sgrMail {
+	
+
+	private $subject =	array(	'addEvento'		=> 'Nueva solicitud de reserva',
+								'editEvento'	=> 'Modificación de solicitud de reserva',
+								'delEvento'		=> 'Eliminación de evento',
+								'aprobar'		=> 'Solicitud aprobada',
+								'denegar'		=> 'Solicitud denegada',
+								'registro'		=> 'Nueva solicitud de registro pendiente',
+								'contacto'		=> 'Notificación formulario de contacto',
+								'activacion'	=> 'Usuario UVUS activado en SGR (Sistema de Gestión de Reservas fcom)',
+								'deshabilita'	=> 'Espacio o medio deshabilitado',
+								'habilita'		=> 'Espacio o medio habilitado',
+							);
+
+	
+	public function notificaDeshabilitaRecurso($id){
+
+		if (empty($id)) return true;
+
+		//Subject 
+		$s = date('d-m-Y H:i') .': '.$this->subject['deshabilita'];
+		//Notifica a todos los usuarios con reservas futuras en el recurso deshabilitado
+		$eventos = Evento::where('recurso_id','=',$id)->get();
+		$eventosToMail = $eventos->filter(function($evento){
+			return strtotime($evento->fechaEvento) >= strtotime('today');
+		});
+
+		foreach ($eventosToMail as $evento) {
+			
+			$data = array('evento' => serialize($evento));
+
+			if (!empty($evento->userOwn->email)){
+				$mailUser = $evento->userOwn->email;
+				Mail::queue(array('html'=>'emails.deshabilitaRecurso'),$data,function($m) use ($mailUser,$s){
+				$m->to($mailUser)->subject($s);});
+			}//fin if		
+		}//fin foreach			
+	}//fin notificaDeshabilitaRecurso
+	
+	public function notificaHabilitaRecurso($id){
+
+		if (empty($id)) return true;
+
+		//Subject 
+		$s = date('d-m-Y H:i') .': '.$this->subject['habilita'];
+		//Notifica a todos los usuarios con reservas futuras en el recurso deshabilitado
+		$eventos = Evento::where('recurso_id','=',$id)->get();
+		$eventosToMail = $eventos->filter(function($evento){
+			return strtotime($evento->fechaEvento) >= strtotime('today');
+		});
+
+		foreach ($eventosToMail as $evento) {
+			
+			$data = array('evento' => serialize($evento));
+
+			if (!empty($evento->userOwn->email)){
+				$mailUser = $evento->userOwn->email;
+				Mail::queue(array('html'=>'emails.habilitaRecurso'),$data,function($m) use ($mailUser,$s){
+				$m->to($mailUser)->subject($s);});
+			}//fin if		
+		}//fin foreach			
+	}//fin notificahabilitaRecurso	
+
+
+	
+
+	public function notificaActivacionUVUS($idUser){
+
+		//Subject 
+		$s = date('d-m-Y H:i') .': '.$this->subject['activacion'];
+		//Notifica solicitante
+		$user = User::find($idUser);
+		$data = array('user' => serialize($user));
+
+		if (!empty($user)){
+			$mailUser = $user->email;
+			if ( !empty($mailUser) ){
+				Mail::queue(array('html'=>'emails.activaUvus'),$data,function($m) use ($mailUser,$s){
+				$m->to($mailUser)->subject($s);
+			});
+			}		
+		}
+		
+		
+	}
+
+	public function notificaValidacion($acccion,$evento_id){
+
+		
+		$evento = Evento::where('evento_id','=',$evento_id)->first();//datos de la solicitud de uso
+		
+		$data = array('evento' => serialize($evento),'validador' => Auth::user()->nombre .' '. Auth::user()->apellidos);
+		$s = date('d-m-Y H:i') .': '. $evento->recursoOwn->nombre . ': '. $this->subject[$acccion];
+		
+		$validadores = User::where('capacidad','=',5)->get(); //Todos los Validadores incluido Auth::user() (validador autenticado)
+		//Notifica validadores
+		foreach ($validadores as $validador) {
+			if ( !empty($validador->email) )
+				Mail::queue(array('html' => 'emails.validacion'),$data,function($m) use ($validador,$s){
+					$m->to($validador->email)->subject($s);
+				});	
+		}
+		
+		//Notifica solicitante
+		$mailSolicitante = $evento->userOwn->email;
+		if ( !empty($mailSolicitante) ){
+			Mail::queue(array('html'=>'emails.validacion'),$data,function($m) use ($mailSolicitante,$s){
+				$m->to($mailSolicitante)->subject($s);
+			});
+		}
+
+		//Notifica técnicos
+		if ($this->notificaTecnicos($evento->fechaInicio)) //notificamos	
+		
+	
+	}//fin notificaValidacion
+
+	private function notificaTecnicos($fechaInicio){
+		
+		$notifica = false;
+
+		$minimodias = Config::get('options.ant_minDias');
+		$tsMinimoDiasAntelacion = $minimodias * 24 * 60 * 60;
+		$numSemanasMinimasDeAntelacion = Config::get('options.ant_minSemanas');
+
+		if (  (strtotime($fechaInicio) - strtotime('today') ) <= $tsMinimoDiasAntelacion )
+			$notifica = true;
+		if ( (date('W',strtotime($fechaInicio)) - date('W') ) < $numSemanasMinimasDeAntelacion ) 
+			$notifica = true;
+		return $notifica;
+
+	}
+
+	public function notificaNuevoEvento($evento){
+
+		$data = array('evento' => serialize($evento),'solicitante' => Auth::user()->nombre .' '. Auth::user()->apellidos );
+		$s = 	date('d-m-Y H:i') .': '. $this->subject['addEvento'] . ' en ' . $evento->recursoOwn->nombre;
+		$validadores = User::where('capacidad','=',5)->get(); //todos  los validadores
+		//Notifica validadores
+		foreach ($validadores as $validador) {
+			if ( !empty($validador->email) )
+				Mail::queue(array('html' => 'emails.detalleReserva'),$data,function($m) use ($validador,$s){
+					$m->to($validador->email)->subject($s);
+				});	
+		}
+
+
+	}//fin notificaNuevoEvento
+
+	public function notificaEdicionEvento($evento){
+
+		$data = array('evento' => serialize($evento),'solicitante' => Auth::user()->nombre .' '. Auth::user()->apellidos );
+		$s = 	date('d-m-Y H:i') .': '. $this->subject['editEvento'] . ' en ' . $evento->recursoOwn->nombre;
+		$validadores = User::where('capacidad','=',5)->get(); //todos  los validadores
+		//Notifica validadores
+		foreach ($validadores as $validador) {
+			if ( !empty($validador->email) )
+				Mail::queue(array('html' => 'emails.detalleReserva'),$data,function($m) use ($validador,$s){
+					$m->to($validador->email)->subject($s);
+				});	
+		}
+
+
+	}//fin notificaEdicionEvento
+	/***
+		@param in $user=array('nombre','apellidos','uvus','relacionUSES','colectivo','ubicacion')
+		//todos los valores devueltos por sso
+	*/
+	public function notificaRegistroUser($user){
+
+		$s = date('d-m-Y H:i') .': '.$this->subject['registro'];
+		$data = array('user' => serialize($user));
+		$administradores = User::where('capacidad','=','4')->get();//Todos los administradores
+		foreach ($administradores as $administrador) {
+				if ( !empty($administrador->email) )
+					Mail::queue(array('html' => 'emails.registroNuevoUsuario'),$data,function($m) use($administrador,$s){
+							$m->to($administrador->email)->subject($s);
+					});
+		}
+	}
+
+	public function notificaContacto($data){
+
+		$s = date('d-m-Y H:i') .': '. $this->subject['contacto'];
+		$administradores = User::where('capacidad','=',4)->get();//Todos los administradores
+		foreach ($administradores as $administrador) {
+			if ( !empty($administrador->email) )
+				Mail::queue(array('html' => 'emails.contacto'),$data,function($m) use($administrador,$s){
+					$m->to($administrador->email)->subject($s);
+				});
+		}
+	}//fin function notificaContacto
+
+
+}//fin sgrMail
+
+?>
